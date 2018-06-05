efe364c8f24bc6becfe53b36c7bc65ef6b02ea7c
Danichu300 <danikillersx@gmail.com>
2018-06-05 08:07:51 +0000

    Quitar comentario

--- archivos_php/navegador.php
+++ archivos_php/navegador.php
@@ -26,7 +26,7 @@
                             </form>
                         </div>
                     </li>
-                    <?php//asdjfhlkjashdfkljhaskjdhfkahsdfkjhasdkjfhakjshdf sha matao Paco!!!!!!
+                    <?php
                     session_start();
                     if (isset($_SESSION['nombre_usuario'])) {
                         echo "<li class='nav-item dropdown'>";
